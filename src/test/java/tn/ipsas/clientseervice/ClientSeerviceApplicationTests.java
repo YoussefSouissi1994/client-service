62cc5d198a93b7c3faf1c7ff86ea2ceff3fc5027
Youssef souissi <youssefsouissi21@gmail.com>
2022-12-25 13:07:09 +0100

    first commit

---
+++ src/test/java/tn/ipsas/clientseervice/ClientSeerviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.ipsas.clientseervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSeerviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
